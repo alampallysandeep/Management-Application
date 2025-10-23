859794a2ec17f77ca240c4cdccc7f098167566ab
alampallysandeep <alampallysandeep812@gmail.com>
2025-10-23 22:06:35 +0530

    project changes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,14 @@ import { Component, OnInit, signal } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit,AfterViewInit{
   currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
+  private destroyRef = inject(DestroyRef);
 
   constructor() { }
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT')
+    const interval = setInterval(() => {
       const rnd = Math.random() // generate values between 0 to 0.99999
       if (rnd < 0.5) {
         this.currentStatus.set('online')
@@ -22,5 +24,15 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus.set('unknown')
       }
     }, 5000);
+    this.destroyRef.onDestroy(()=>{ // alternative to ngOnDestroy hook
+      clearInterval(interval)
+    })
   }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+  }
+  // ngOnDestroy(): void {
+  //   clearInterval(this.interval)
+  // }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>; // to access element with template variable through ViewChild
+  onSubmit(titleElement: string, requestText: string) { // HTMLInputElement is type of an element if you entire element in a function
+    console.log(titleElement)
+    console.log(requestText)
+    this.form?.nativeElement.reset()
+  }
 }
